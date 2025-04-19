4fb42d65122307cc55b08ab7f599c4b06a8d17d2
Ayyoub EL Kouri <Ayyoubelkouri@gmail.com>
2025-04-19 22:40:05 +0100

    feat(Utils): add Enumations file

---
+++ Utils/Enums.h
@@ -0,0 +1,27 @@
+/***************************************************************************************************
+ * @file Enums.h                                                                                   *
+ * @brief Defines all the enums used in the project                                                *
+ *                                                                                                 *
+ * @author Ayyoub EL KOURI                                                                         *
+ * @date 2025-04-19                                                                                *
+ * @version 1.0                                                                                    *
+ *                                                                                                 *
+ * @copyright Copyright (c) 2025, Ayyoub EL KOURI                                                  *
+ *                                                                                                 *
+ * @see Enums.c                                                                                  *
+ **************************************************************************************************/
+
+#ifndef ENUMS_H
+#define ENUMS_H
+
+typedef enum
+{
+    window,
+    headerBar,
+    box,
+    grid,
+    label,
+    button
+} widgetType;
+
+#endif // ENUMS_H
